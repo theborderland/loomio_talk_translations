c45243bf6c013f8fe82932db00af576704923c2a
Hugi <hugiasgeirsson@gmail.com>
2019-05-06 00:09:33 +0200

    Updated to only contain changed strings

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,7 @@ module Plugins
     class Plugin < Plugins::Base
       setup! :talk_translations do |plugin|
         plugin.enabled = true
+        plugin.use_view_path :"app/views"
         plugin.use_translations 'config/locales', :talk
       end
     end
